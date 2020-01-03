bc6b3ec18c5ffdef5ec08b7fb4e984668b89b699
amirali.amajdi <42503768+amirali76@users.noreply.github.com>
2020-01-04 00:32:18 +0330

    Initial commit

---
+++ api/objects/user.php
@@ -0,0 +1,101 @@
+<?php
+// 'user' object
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $user_id;
+    public $username;
+    public $password;
+    public $proffessor;
+ 
+    // constructor
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+// create new user record
+function create(){
+ 
+    // insert query
+    $query = "INSERT INTO " . $this->table_name . "
+            SET
+                username = :username,
+                password = :password,
+                proffessor = :proffessor";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->username=htmlspecialchars(strip_tags($this->username));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+    $this->proffessor=htmlspecialchars(strip_tags($this->proffessor));
+ 
+    // bind the values
+    $stmt->bindParam(':username', $this->username);
+    $stmt->bindParam(':proffessor', $this->proffessor);
+ 
+    // hash the password before saving to database
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password_hash);
+ 
+    // execute the query, also check if query was successful
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+ 
+// check if given username exist in the database
+function usernameExists(){
+ 
+    // query to check if username exists
+    $query = "SELECT user_id, username, password, proffessor
+            FROM " . $this->table_name . "
+            WHERE username = ?
+            LIMIT 0,1";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->username=htmlspecialchars(strip_tags($this->username));
+ 
+    // bind given email value
+    $stmt->bindParam(1, $this->username);
+ 
+    // execute the query
+    $stmt->execute();
+ 
+    // get number of rows
+    $num = $stmt->rowCount();
+ 
+    // if email exists, assign values to object properties for easy access and use for php sessions
+    if($num>0){
+ 
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->user_id = $row['user_id'];
+        $this->username = $row['username'];
+        $this->password = $row['password'];
+        $this->proffessor = $row['proffessor'];
+ 
+        // return true because username exists in the database
+        return true;
+    }
+ 
+    // return false if username does not exist in the database
+    return false;
+}
+ 
+// update() method will be here
+
+
+}
